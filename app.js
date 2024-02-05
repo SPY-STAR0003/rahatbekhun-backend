58d132df81d1b54856a2d25a4592ab7797389787
Mohammad <spystar0003@gmail.com>
2024-02-05 15:29:01 +0330

    Add User Controller

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+// ? ===================== Libraries =====
+const envConfig = require('dotenv').config({path : "./config/config.env"});
+const parser = require('body-parser');
+const connectDB = require('./config/db');
+
+// ? ===================== Express =======
 const express = require('express')
 const app = express()
 
+// ? ===================== Routes ========
+const adminRoute = require('./routes/admin');
 
-const port = 5000
+// * ========= Connect DataBase ==========
+connectDB();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// * ========= Parsers ===================
+app.use(parser.urlencoded({extended : true}))
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+app.use("/admin", adminRoute)
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,16 @@
 
+const { Router } = require('express');
+const router = new Router();
 
-const app = require('express');
+const adminCtrls = require('./../controllers/admin');
+
+router.post("/register", adminCtrls.addUser)
+
+router.post("/login", (req, res) => {
+    res.status(200).json({
+        message : "Hello World !"
+    })
+})
+
+
+module.exports = router
